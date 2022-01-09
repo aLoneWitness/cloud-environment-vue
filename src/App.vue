534e11f2a613d0eac8497946103ea4275b3f42c8
Sander van Dinteren <sanvandinteren@gmail.com>
2022-01-09 16:59:52 +0100

    everything

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,73 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <head>
+      <link rel="stylesheet" href="node_modules/xterm/css/xterm.css" />
+    </head>
+    <button @click="sendMessage()">Test WS</button>
+    <div id="terminal"></div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { io } from 'socket.io-client'
+import { Terminal } from 'xterm'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  created() {
+    this.socket = io('http://localhost:3000')
+    this.socket.on("connect", () => {
+      console.log(this.socket.connected); // true
+      window.setInterval(this.triggerHearthbeat, 5000);
+    });
+  },
+  mounted() {
+    var term = new Terminal();
+    term.open(document.getElementById('terminal'))
+
+    let data = 'DQ==';
+    let buff = new Buffer(data, 'base64');
+    let text = buff.toString('ascii');
+    console.log('b64 decode: ' + text)
+
+    this.socket.on('stdout', (raw) => {
+      // TODO: bruh
+      let stringyJson = JSON.parse(raw)
+      let json = JSON.parse(stringyJson)
+      let decodedStdout = atob(json.stdout.data);
+      term.write(decodedStdout)
+    })
+
+    term.onData((data) => {
+      let buff = new Buffer(data);
+      let base64data = buff.toString('base64');
+      this.socket.emit('stdin', { stdin: { data: base64data } })
+    });
+
+    // term.onLineFeed((data) => {
+    //   console.log(data)
+    // })
+  },
+  methods: {
+    sendMessage() {
+      console.log('sending')
+      // const lol = {
+      //   stdin: {
+      //     data: "DQ=="
+      //   }
+      // }
+      let buff = new Buffer('/r');
+      let base64data = buff.toString('base64');
+      const lol = {
+        stdin: {
+          data: base64data
+        }
+      }
+      this.socket.emit('stdin', lol)
+    },
+    triggerHearthbeat() {
+      this.socket.emit('heartbeat', {})
+    }
   }
 }
 </script>
